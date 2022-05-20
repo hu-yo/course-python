77b0c905a75d71510915d50e3ca940a965aed764
hu-yo <105497379+hu-yo@users.noreply.github.com>
2022-05-20 20:39:10 +0800

    Add files via upload

--- 5-13-3.py
+++ 5-13-3.py
@@ -1,17 +1,40 @@
-def add(x):
+def mul(x):
     z=1
     for a in range(1,x+1):
         z=z*a
     return z
 
-z=add(10)
+z=mul(10)
 print(z)
 
-def ad(x):
+def add(x):
     t=0
     for a in range(1,x+1):
         t=t+a
     return t
 
-t=ad(10)
+def calcall(value, func):
+    return func(value)
+t=add(10)
 print(t)
+print(calcall(10,add))
+print(calcall(10,mul))
+
+
+
+
+def mu(x):
+    z=1
+    for a in x:
+        z=z*a
+    return z
+
+def ad(x):
+    t=0
+    for a in x:
+        t=t+a
+    return t
+x=[1,2,5]
+funcs=[ad,mu]
+for f in funcs:
+    print(calcall(x,f))
